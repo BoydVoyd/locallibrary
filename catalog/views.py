0e4e3434f316e03e885f0b22b0bcbeb2edfb8c13
Nathan Davis <nathanwdavis@gmail.com>
2025-07-05 13:55:23 -0500

    added challeng exercises

--- catalog/views.py
+++ catalog/views.py
@@ -17,11 +17,19 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    # Number of Books with Dune in the title
+    num_dune = Book.objects.filter(title__icontains="dune").count()
+
+    # Number of Genres with Fiction in the name
+    num_fiction = Genre.objects.filter(name__icontains="fiction").count()
+
     context = {
         "num_books": num_books,
         "num_instances": num_instances,
         "num_instances_available": num_instances_available,
         "num_authors": num_authors,
+        "num_dune": num_dune,
+        "num_fiction": num_fiction,
     }
 
     # Render the HTML template index.html with the data in the context variable
